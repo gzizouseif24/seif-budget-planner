a504b2ff367509b1c51b72127fc927cca1652b04
gzizou24 <gzizou.seif@gmail.com>
2025-05-12 16:05:49 +0100

    build

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Import Page Components
 import DashboardPage from './pages/DashboardPage';
